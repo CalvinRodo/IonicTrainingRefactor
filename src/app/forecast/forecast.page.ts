5ab77660dd2cb174795fc869a3f9e18e931225fa
CalvinRodo <calvinv@gmail.com>
2018-09-21 15:09:14 -0400

    Fixing pages

--- src/app/forecast/forecast.page.ts
+++ src/app/forecast/forecast.page.ts
@@ -1,8 +1,57 @@
 import { Component } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import { Forecast } from '../models/forecast';
+import { IconMapService } from '../services/icon-map/icon-map.service';
+import { WeatherService } from '../services/weather/weather.service';
+import { ModalController } from '@ionic/angular';
+
+import { UserPreferencesComponent } from '../user-preferences/user-preferences.component';
+import { UserPreferencesService } from '../services/user-preferences/user-preferences.service';
+
 @Component({
   selector: 'app-forecast',
   templateUrl: 'forecast.page.html',
   styleUrls: ['forecast.page.scss']
 })
-export class ForecastPage {}
+export class ForecastPage {
+
+  forecast: Forecast;
+  scale: string;
+
+  private subscription: Subscription;
+
+  constructor(
+    public iconMap: IconMapService,
+    private modal: ModalController,
+    private userPreferences: UserPreferencesService,
+    private weather: WeatherService
+  ) {}
+
+  ionViewDidLoad() {
+    this.subscription = this.userPreferences.changed.subscribe(() =>
+      this.getData()
+    );
+  }
+
+  ionViewDidEnter() {
+    this.getData();
+  }
+
+  ionViewWillUnload() {
+    this.subscription.unsubscribe();
+  }
+
+  async openUserPreferences() {
+    const m = await this.modal.create({
+      component: UserPreferencesComponent
+    });
+    return await m.present();
+  }
+
+  private getData() {
+    this.userPreferences.getUseCelcius().then(u => { this.scale = u ? 'C' : 'F'; });
+    this.weather.forecast().subscribe(f => (this.forecast = f));
+  }
+}
--- src/app/uv-index/uv-index.page.ts
+++ src/app/uv-index/uv-index.page.ts
@@ -1,8 +1,68 @@
 import { Component } from '@angular/core';
+import { UVIndex } from '../models/uv-index';
+import { ModalController } from '@ionic/angular';
+
+import { Subscription } from 'rxjs';
+
+import { UserPreferencesComponent } from '../user-preferences/user-preferences.component';
+import { UserPreferencesService } from '../services/user-preferences/user-preferences.service';
+import { WeatherService } from '../services/weather/weather.service';
 
 @Component({
   selector: 'app-uv-index',
   templateUrl: 'uv-index.page.html',
-  styleUrls: ['uv-index.page.scss']
+  styleUrls: ['uv-index.page.scss'],
 })
-export class UVIndexPage {}
+export class UVIndexPage {
+  uvIndex: UVIndex;
+
+  advice: Array<string> = [
+    'Wear sunglasses on bright days. If you burn easily, cover up and use broad spectrum SPF 30+ sunscreen. ' +
+      'Bright surfaces, such as sand, water and snow, will increase UV exposure.',
+    'Stay in the shade near midday when the sun is strongest. If outdoors, wear sun protective clothing, ' +
+      'a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, ' +
+      'even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water and snow, will increase UV exposure.',
+    'Reduce time in the sun between 10 a.m. and 4 p.m. If outdoors, seek shade and wear sun protective clothing, a wide-brimmed hat, ' +
+      'and UV-blocking sunglasses. Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, ' +
+      'and after swimming or sweating. Bright surfaces, such sand, water and snow, will increase UV exposure.',
+    'Minimize sun exposure between 10 a.m. and 4 p.m. If outdoors, seek shade and wear sun protective clothing, a wide-brimmed hat, ' +
+      'and UV-blocking sunglasses. Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after ' +
+      'swimming or sweating. Bright surfaces, such as sand, water and snow, will increase UV exposure.',
+    'Try to avoid sun exposure between 10 a.m. and 4 p.m. If outdoors, seek shade and wear sun protective clothing, a wide-brimmed hat, ' +
+      'and UV-blocking sunglasses. Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, ' +
+      'and after swimming or sweating. Bright surfaces, such as sand, water and snow, will increase UV exposure.',
+  ];
+
+  private subscription: Subscription;
+
+  constructor(
+    private modal: ModalController,
+    private userPreferences: UserPreferencesService,
+    private weather: WeatherService
+  ) {}
+
+  ionViewDidLoad() {
+    this.subscription = this.userPreferences.changed.subscribe(() =>
+      this.getData()
+    );
+  }
+
+  ionViewDidEnter() {
+    this.getData();
+  }
+
+  ionViewWillUnload() {
+    this.subscription.unsubscribe();
+  }
+
+  async openUserPreferences() {
+    const m = await this.modal.create({
+      component: UserPreferencesComponent,
+    });
+    return await m.present();
+  }
+
+  private getData() {
+    this.weather.uvIndex().subscribe(i => (this.uvIndex = i));
+  }
+}
